5c293360082556ff9105775f0fec10a484782c8f
julien huang <julien.huang@outlook.fr>
2023-03-12 19:15:27 +0100

    test: reproduction of the issue

---
+++ packages/nuxt/src/app/components/cachetest.ts
@@ -0,0 +1,11 @@
+
+const cacheTest: any[] = []
+export const setTest = new Set<any>()
+
+export const nuxtappCache : any[] = []
+export const nuxtAppSet = new Set<any>()
+
+export const vmNuxtApp : any[] = []
+export const vmAppSet = new Set<any>()
+
+export default cacheTest
--- packages/nuxt/src/app/components/nuxt-root.vue
+++ packages/nuxt/src/app/components/nuxt-root.vue
@@ -7,18 +7,27 @@
 </template>
 
 <script setup>
-import { defineAsyncComponent, onErrorCaptured, onServerPrefetch, provide } from 'vue'
-import { callWithNuxt, useNuxtApp } from '#app/nuxt'
+import { defineAsyncComponent, onErrorCaptured, onServerPrefetch, provide, getCurrentInstance } from 'vue'
+import  cacheTest,{ setTest, nuxtAppSet, nuxtappCache  } from '#app/components/cachetest'
+import { callWithNuxt } from '#app/nuxt'
 import { isNuxtError, showError, useError } from '#app/composables/error'
 import { useRoute } from '#app/composables/router'
 import AppComponent from '#build/app-component.mjs'
+const vm = getCurrentInstance()
+cacheTest.push(vm.appContext.app.$nuxt)
+setTest.add(vm.appContext.app.$nuxt)
+console.log(setTest.size, cacheTest.length, 'array -- set of nuxt app of the current instance in nuxt-root')
+const nuxtApp = useNuxtApp()
+nuxtappCache.push(nuxtApp)
+nuxtAppSet.add(nuxtApp)
+console.log( nuxtappCache.length, nuxtAppSet.size, 'array -- set of nuxt app instance in nuxt-root')
+
 
 const ErrorComponent = defineAsyncComponent(() => import('#build/error-component.mjs').then(r => r.default || r))
 const IslandRenderer = process.server
   ? defineAsyncComponent(() => import('./island-renderer').then(r => r.default || r))
   : () => null
 
-const nuxtApp = useNuxtApp()
 const onResolve = nuxtApp.deferHydration()
 
 // Inject default route (outside of pages) as active route
